e79d2cf99323d84d3a841a35f3457ce44df16845
Azwad <mdazwad22@gmail.com>
2024-11-18 05:31:53 +0600

    [ADD] internal/routes : create_order route added with middleware

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -3,14 +3,17 @@ package routes
 import (
 	"github.com/gorilla/mux"
 	"go-application-task/internal/handlers"
+	"go-application-task/internal/middleware"
 )
 
 func SetupRoutes() *mux.Router {
 	router := mux.NewRouter()
 
-	// login route
 	router.HandleFunc("/login", handlers.LoginHandler).Methods("POST")
 	router.HandleFunc("/refresh", handlers.RefreshTokenHandler).Methods("POST")
+
+	createOrderRoute := router.HandleFunc("/create_order", handlers.CreateOrderHandler).Methods("POST")
+	createOrderRoute.Handler(middleware.JWTMiddleware(createOrderRoute.GetHandler()))
 
 	return router
 }
