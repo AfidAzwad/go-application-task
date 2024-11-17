f9e554657f8778d88650fff06cda32c14cb27838
Azwad <mdazwad22@gmail.com>
2024-11-18 05:32:59 +0600

    [UPDATE] go.mod and main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/jmoiron/sqlx"
+	"go-application-task/internal/middleware"
 	"go-application-task/internal/routes"
 	"log"
 	"net/http"
@@ -8,24 +10,27 @@ import (
 	"go-application-task/pkg/db"
 )
 
+// Helper function to close a database connection with proper error handling
+func closeDatabaseConnection(dbConnection *sqlx.DB, dbName string) {
+	if err := dbConnection.Close(); err != nil {
+		log.Printf("Error closing %s: %v", dbName, err)
+	} else {
+		log.Printf("%s closed successfully", dbName)
+	}
+}
+
 func main() {
 	if err := db.InitDB(); err != nil {
 		log.Fatalf("Failed to initialize databases: %v", err)
 	}
 
-	defer func() {
-		if err := db.WriteDB.Close(); err != nil {
-			log.Printf("Error closing WriteDB: %v", err)
-		}
-	}()
-	defer func() {
-		if err := db.ReadDB.Close(); err != nil {
-			log.Printf("Error closing ReadDB: %v", err)
-		}
-	}()
+	// Ensure proper closing of the database connections after the application exits
+	defer closeDatabaseConnection(db.WriteDB, "WriteDB")
+	defer closeDatabaseConnection(db.ReadDB, "ReadDB")
 
 	router := routes.SetupRoutes()
+	routerWithCors := middleware.EnableCors(router)
 
 	log.Println("Server running on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":8080", routerWithCors))
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,9 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gorilla/mux v1.8.1
 	github.com/jmoiron/sqlx v1.4.0
-	github.com/lib/pq v1.10.9 // indirect
+	github.com/lib/pq v1.10.9
+	github.com/rubenv/sql-migrate v1.3.1
 	golang.org/x/crypto v0.29.0
 )
+
+require github.com/rs/cors v1.11.1 // indirect
